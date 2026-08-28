ded170edcca6a76e4ff1db68df3c959776625210
Lokachop <lokachop@gmail.com>
2026-08-28 16:31:46 +0200

    fix the previews

--- tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/MainPage.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/MainPage.tsx
@@ -530,15 +530,10 @@ export function MainPage(props: MainPageProps) {
   enum PrefPage {
     Visual, // The visual parts
     Profile, // Flavor Text, Age, Records, PDA ringtone, etc
-    ERP, // ERP Prefs
   }
 
   const [currentPrefPage, setCurrentPrefPage] = useState(PrefPage.Visual);
-  const erpEnabled = !!data.erp_pref;
-  const filteredCurrentPrefPage =
-    currentPrefPage === PrefPage.ERP && !erpEnabled
-      ? PrefPage.Visual
-      : currentPrefPage;
+  const filteredCurrentPrefPage = currentPrefPage
 
   let prefPageContents;
   switch (filteredCurrentPrefPage) {
@@ -568,19 +563,6 @@ export function MainPage(props: MainPageProps) {
         />
       );
       break;
-    case PrefPage.ERP:
-    prefPageContents = (
-      <PreferenceList
-        randomizations={getRandomization(
-          erpPreferences,
-          serverData,
-          randomBodyEnabled,
-        )}
-        preferences={erpPreferences}
-        maxHeight="auto"
-      />
-    );
-    break;
     default:
       exhaustiveCheck(filteredCurrentPrefPage);
   }
@@ -779,17 +761,6 @@ export function MainPage(props: MainPageProps) {
                   Character Profile
                 </PageButton>
               </Stack.Item>
-             {erpEnabled && (
-              <Stack.Item grow={0.5}>
-                <PageButton
-                  currentPage={currentPrefPage}
-                  page={PrefPage.ERP}
-                  setPage={setCurrentPrefPage}
-                >
-                <Icon name="heart" />
-                </PageButton>
-              </Stack.Item>
-            )}
             </Stack>
             {prefPageContents}
           </Stack>
